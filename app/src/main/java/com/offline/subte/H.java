24c49b5a0e4a126bc430b46db56910641ea4c81f
Cnc <cristian_dev@pmovil.com.ar>
2016-04-13 18:26:38 -0300

    Initial commit

---
+++ app/src/main/java/com/offline/subte/H.java
@@ -0,0 +1,16 @@
+package com.offline.subte;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Cnc on 13/04/2016.
+ */
+public class H extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.h);
+    }
+}
---
+++ app/src/main/java/com/offline/subte/MainActivity.java
@@ -0,0 +1,54 @@
+package com.offline.subte;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void btnA(View v)
+    {
+        Intent intent = new Intent(getApplicationContext(), A.class);
+        startActivity(intent);
+    }
+
+    public void btnB(View v)
+    {
+        Intent intent = new Intent(getApplicationContext(), B.class);
+        startActivity(intent);
+    }
+
+    public void btnC(View v)
+    {
+        Intent intent = new Intent(getApplicationContext(), C.class);
+        startActivity(intent);
+    }
+
+    public void btnD(View v)
+    {
+        Intent intent = new Intent(getApplicationContext(), D.class);
+        startActivity(intent);
+    }
+
+    public void btnE(View v)
+    {
+        Intent intent = new Intent(getApplicationContext(), E.class);
+        startActivity(intent);
+    }
+
+    public void btnH(View v)
+    {
+        Intent intent = new Intent(getApplicationContext(), H.class);
+        startActivity(intent);
+    }
+
+
+}
